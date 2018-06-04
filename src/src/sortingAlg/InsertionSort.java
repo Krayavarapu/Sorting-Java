ae8f8f2f7f7d0b4bd80c1435d63c299f9cc8feec
Krayavarapu <krayavarapu198@gmail.com>
2018-06-04 00:17:21 -0400

    Implemented algorithm for Insertion Sort

--- src/src/sortingAlg/InsertionSort.java
+++ src/src/sortingAlg/InsertionSort.java
@@ -2,9 +2,23 @@ package sortingAlg;
 
 import java.util.ArrayList;
 
-public class InsertionSort {
+public class InsertionSort extends Sort {
 
-    public ArrayList<Integer> sort(ArrayList<Integer> list) {
-        return null;
+    @Override
+    public void sort(int list[]) {
+        //int low = 0;
+        int high = list.length;
+        for (int i = 1; i < high; i++) {
+            int key = list[i];
+            int j = i-1;
+
+            while (j >= 0 && key < list[j]) {
+                list[j+1] = list[j];
+                j = j - 1;
+            }
+            list[j+1] = key;
+        }
+
     }
+
 }

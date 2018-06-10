1982ac27bbc73e947ba5f0a5cfd3fdf7e557df83
Krayavarapu <krayavarapu198@gmail.com>
2018-06-10 16:18:03 -0400

    Implemenmted sendToGivenAlgorithm and isChosen methods in OptionThree class

--- src/src/Selections/OptionThree.java
+++ src/src/Selections/OptionThree.java
@@ -1,4 +1,25 @@
 package Selections;
 
+import sortingAlg.QuickSort;
+
 public class OptionThree {
+
+    protected int val[];
+    boolean chosen;
+
+    public OptionThree(int arr[]) {
+        val = arr;
+        chosen = false;
+        sendToGivenAlgorithm(val);
+    }
+
+    public void sendToGivenAlgorithm(int arr[]) {
+        QuickSort newSort = new QuickSort();
+        newSort.sort(arr);
+    }
+
+    public boolean isChosen() {
+        chosen = true;
+        return chosen;
+    }
 }

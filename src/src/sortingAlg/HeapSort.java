900d31db9a9bfcf7aec1952a26320e45da797192
Krayavarapu <krayavarapu198@gmail.com>
2018-06-10 13:06:26 -0400

    Implemented HeapSort algorithm

--- src/src/sortingAlg/HeapSort.java
+++ src/src/sortingAlg/HeapSort.java
@@ -2,9 +2,46 @@ package sortingAlg;
 
 import java.util.ArrayList;
 
-public class HeapSort {
+public class HeapSort extends Sort {
 
-    public ArrayList<Integer> sort(ArrayList<Integer> list) {
-        return null;
+    @Override
+    public void sort(int arr[]) {
+
+        int x = arr.length;
+
+        for (int i = x/2 - 1; i >= 0; i--) {
+            heapify(arr, x, i);
+        }
+
+        for (int i = x-1; i>= 0; i--) {
+
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    public void heapify(int arr[], int n, int i) {
+        int largestVal = i;
+        int left = 2*i + 1;
+        int right = 2*i + 2;
+
+        if (left < n && arr[left] > arr[largestVal]) {
+            largestVal = left;
+        }
+
+        if (right < n && arr[right] > arr[largestVal]) {
+            largestVal = right;
+        }
+
+        if (largestVal != i) {
+            int swap = arr[i];
+            arr[i] = arr[largestVal];
+            arr[largestVal] = swap;
+
+            heapify(arr, n, largestVal);
+        }
     }
 }
--- src/src/sortingAlg/InsertionSort.java
+++ src/src/sortingAlg/InsertionSort.java
@@ -1,6 +1,6 @@
 package sortingAlg;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class InsertionSort extends Sort {
 

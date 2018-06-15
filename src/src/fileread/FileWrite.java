35abe4c2dae4a75d546739b6d13792dc4ac282f3
Krayavarapu <krayavarapu198@gmail.com>
2018-06-15 18:01:40 -0400

    Created FileWrite Class

---
+++ src/src/fileread/FileWrite.java
@@ -0,0 +1,45 @@
+package fileread;
+
+//import java.io.File;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+
+public class FileWrite {
+
+    //File file;
+
+    PrintWriter pw;
+
+    protected int[] array;
+
+    public FileWrite(int[] arr) {
+        array = arr;
+        writeToFile(array);
+    }
+
+    public PrintWriter writeToFile(int[] arr) {
+
+        //File fileWrite = new File();
+        try {
+            pw = new PrintWriter("Sorted_Values.txt", "UTF-8");
+            for (int i = 0; i < arr.length; i++) {
+                pw.print(arr[i]);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return pw;
+    }
+
+
+
+
+
+
+}
